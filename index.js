1e58142d3de091c0619145cfe50bc434ff9c663b
Leonardo Chavez <leonardchavez05@gmail.com>
2023-08-27 18:04:48 +0800

    Added saved list feature(unfinished)

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const inputEl = document.getElementById("input-el")
 const trackerEl = document.getElementById("tracker-el")
 const enterBtn = document.getElementById("enter-btn")
 const clearBtn = document.getElementById("clear-btn")
+const saveList = document.getElementById("save-list")
 
+let listItems = []
 let adder = 0
 
 enterBtn.addEventListener("click", function(){
@@ -12,9 +14,13 @@ enterBtn.addEventListener("click", function(){
         adder += input
         inputEl.value = ""
         trackerEl.innerHTML = `Tracker <br/> ${adder} `
+        listItems.push(input)
+        console.log(listItems)
+        saveListItems()
     } else {
         inputEl.value = ""
     }
+    
 
 })
 
@@ -22,3 +28,12 @@ clearBtn.addEventListener("dblclick", function(){
     trackerEl.innerHTML = `Tracker <br/> ... `
     inputEl.value = ""
 })
+
+function saveListItems(){
+    let renderItems = ""
+    for(let i = 0; i < listItems.length; i++){
+        renderItems += `<li> ${listItems[i]} </li>`
+     }
+     console.log(renderItems)
+     saveList.innerHTML = renderItems
+}
